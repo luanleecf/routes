6736db40e1912589d5dc064674d984d3fb5548f4
Luan Lee <luanleedacostafaria@gmail.com>
2022-10-05 11:03:49 -0300

    routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+  <app-menu></app-menu>
+  <router-outlet></router-outlet>`,
 })
-export class AppComponent {
-  title = 'routes';
-}
+export class AppComponent {}
